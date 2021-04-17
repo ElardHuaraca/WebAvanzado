4616dcdfc1c231b62627a94467c1fe1ab50e9bf8
cristhian Lopez <cristhian.lopez@tecsup.edu.pe>
2021-04-17 17:36:44 -0500

    first commit

---
+++ router.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+module.exports.route = function route(request, response) {
+    const object = new URL(request.url, 'http://' + request.headers.host);
+    let ref = 'pages' + object.pathname;
+    if (ref === 'pages/index' || ref === 'pages/') {
+        ref = 'pages/index.html';
+    }
+    fs.stat(ref, fail => {
+        fs.readFile(ref, function (err, content) {
+            var extencion = ref.split('.');
+            var type = '';
+            if (err == null) {
+                switch (extencion.length) {
+                    case 3:
+                        type = 'text/' + extencion[2];
+                        break;
+                    default:
+                        type = 'text/' + extencion[1];
+                        break;
+                }
+                response.writeHead(200, { 'Content-Type': type });
+                response.end(content);
+            } else {
+                ref = 'pages/error.html';
+                fs.readFile(ref,function(err,content) {
+                    response.writeHead(404, { 'Content-Type': 'html' });
+                    response.end(content);
+                })
+            }
+        });
+    });
+    
+}
