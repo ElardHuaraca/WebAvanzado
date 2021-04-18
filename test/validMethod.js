4fc73cc538fe4ce61fb3c28ce2d099105db79e0b
cristhian Lopez <cristhian.lopez@tecsup.edu.pe>
2021-04-17 21:22:15 -0500

    redireccionar con botones

--- test/validMethod.js
+++
@@ -1,9 +0,0 @@
-module.exports.validMethod = function validMethod(method, url, request, response) {
-    var url2 = request.url.split('?');
-    if (("/" + url) === url2[0]) {
-        if (method != request.method) {
-            response.writeHead(405, { "Content-Type": "text/html" });
-            response.end();
-        }
-    }
-}
