8cee9aff0755ebb0fe3848bc94dd143033348a58
unknown <elard.sd.9@gmail.com>
2021-06-07 23:51:04 -0500

    Lab terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,30 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'lab14';
+  title = 'Heroes que pueden volar';
+  heroes: any[] = [];
+  canFly = true;
+  mutate = true;
+  birthDay = new Date();
+
+  power = 5;
+  factor = 1;
+
+  constructor() { this.reset(); console.log(this.birthDay)}
+
+  addHero(name: string) {
+    name = name.trim();
+    if (!name) { return; }
+    let hero = { name, canFly: this.canFly, birthDay: this.birthDay };
+    this.heroes = this.heroes.concat(hero);
+  }
+
+  reset() { this.heroes = []; }
+
+  
+  get format() { return this.mutate ? 'shortDate' : 'fullDate'; }
+
+  change() { this.mutate = !this.mutate; }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlyingHeroesPipe } from './flying-heroes.pipe';
+import { ExponentialStrengthPipe } from './exponential-strength.pipe';
+import { FetchJsonPipePipe } from './fetch-json-pipe.pipe';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlyingHeroesPipe,
+    ExponentialStrengthPipe,
+    FetchJsonPipePipe,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
